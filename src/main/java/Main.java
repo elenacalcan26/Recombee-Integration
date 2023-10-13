546150b43250835aa5973e6e4cc98aa62f12fef7
Elena Dumitru <elena.dumitru0312@stud.acs.upb.ro>
2023-10-13 12:59:18 +0300

    Send data to recombee

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
+import com.recombee.api_client.exceptions.ApiException;
 import dataset.Movie;
 import parser.DatasetParser;
+import recombee.RecombeeClientWrapper;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, ApiException {
         List<Movie> movieDataset = DatasetParser.parseMovieDataset();
+        RecombeeClientWrapper recombeeClientWrapper = new RecombeeClientWrapper((ArrayList<Movie>) movieDataset);
+//        recombeeClientWrapper.sendItemValues();
     }
 }
--- src/main/java/recombee/RecombeeClientWrapper.java
+++ src/main/java/recombee/RecombeeClientWrapper.java
@@ -1,7 +1,14 @@
 package recombee;
 
 import com.recombee.api_client.RecombeeClient;
+import com.recombee.api_client.api_requests.SetItemValues;
+import com.recombee.api_client.exceptions.ApiException;
 import com.recombee.api_client.util.Region;
+import dataset.Movie;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class RecombeeClientWrapper {
     private static String DB_NAME = "acs-sac-dev";
@@ -9,7 +16,30 @@ public class RecombeeClientWrapper {
 
     private RecombeeClient client;
 
-    public RecombeeClientWrapper() {
+    private ArrayList<Movie> movieDataset;
+
+    public RecombeeClientWrapper(ArrayList<Movie> movieDataset) {
         client = new RecombeeClient(DB_NAME, TOKEN).setRegion(Region.EU_WEST);
+        this.movieDataset = movieDataset;
+    }
+
+    public void sendItemValues() throws ApiException {
+        for (Movie movie : movieDataset) {
+            client.send(new SetItemValues(String.valueOf(movie.getRank()),
+                new HashMap<String, Object>() {{
+                    put("title", movie.getTitle());
+                    put("genre", movie.getGenre());
+                    put("description",movie.getDescription());
+                    put("director", movie.getDirector());
+                    put("actors", movie.getActors());
+                    put("year", movie.getYear());
+                    put("runtime", movie.getRuntime());
+                    put("rating", movie.getRating());
+                    put("votes", movie.getVotes());
+                    put("revenue", movie.getRevenue());
+                    put("metascore", movie.getMetascore());
+                }}
+            ).setCascadeCreate(true));
+        }
     }
 }
